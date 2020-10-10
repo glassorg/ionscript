ffc23215e4780993a069162a29130826505c0456
Kris Nye <krisnye@gmail.com>
2020-10-10 01:07:24 -0700

    removed internal runtime

--- src/internal/isPrimitive.ts
+++
@@ -1,8 +0,0 @@
-
-// export function isPrimitive(object) {
-//     if (object == null) {
-//         return true
-//     }
-//     let type = typeof object
-//     return type === "boolean" || type === "number" || type === "string" || type === "bigint" || type === "symbol"
-// }
