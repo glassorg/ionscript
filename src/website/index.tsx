19087fbe4b9dc4941f3614d59311d57270771937
Kris Nye <krisnye@gmail.com>
2020-12-11 20:51:02 -0800

    new docs build technique

--- src/website/index.tsx
+++ src/website/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<h1>Hello React Ionscript Website</h1>, document.getElementById("root"));
+render(<h1>Hello React Ionscript Website 2</h1>, document.getElementById("root"));
